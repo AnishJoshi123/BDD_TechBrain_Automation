927140acf84839dbdd53dbafb854451d7236612a
Anish Joshi <141241575+AnishJoshi123@users.noreply.github.com>
2026-03-19 14:03:50 +0545

    Update steps1.py

--- features/steps/steps1.py
+++ features/steps/steps1.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 import time
 from behave import given, when, then
 from Page1.Dashboard import Dashboard
